b3d9aaf3e0719906b9dd82c9e05df92b726068cf
GYUHHH <gyuhyun0104@gmail.com>
2026-08-14 12:24:38 +0900

    Set mirror upright

--- src/components/InventoryFurniture.tsx
+++ src/components/InventoryFurniture.tsx
@@ -188,7 +188,7 @@ export function ItemVisual({ item, preview = false }: { item: FurnitureItem; pre
       <mesh position={[.06, .95, .31]}><cylinderGeometry args={[.014, .014, .16, 6]} />{mat('#4c4036')}</mesh>
     </Swing>
   </>
-  if (item.type === 'mirror') return <group rotation={[-.09, 0, 0]}>
+  if (item.type === 'mirror') return <group>
     <RoundedBox castShadow args={[.52, 1.52, .06]} radius={.03} smoothness={2} position={[0, .78, 0]}>{mat('#8a6048')}</RoundedBox>
     <mesh position={[0, .78, .033]}><planeGeometry args={[.42, 1.4]} />
       <meshStandardMaterial color={material.color ?? '#cfdce2'} metalness={.7} roughness={.15} transparent={material.transparent} opacity={material.opacity} />
